4d5ea228e4da30f8006d4c5df2a4d4d7b549feba
Facundo Aquino <facun90@live.com>
2021-02-27 21:55:35 -0300

    readDb and  saveTasksFromArray

--- models/Tasks.js
+++ models/Tasks.js
@@ -1,13 +1,10 @@
 const Task = require('./Task')
 
 class Tasks {
-
-	get tasksToArray(){
+	get tasksToArray() {
 		const tasks = []
 
-
-
-		Object.keys(this._all).forEach(key=>{
+		Object.keys(this._all).forEach((key) => {
 			const tsk = this._all[key]
 
 			tasks.push(tsk)
@@ -20,16 +17,17 @@ class Tasks {
 		this._all = {}
 	}
 
-	createTask(description = '') {
-		const task = new Task(description)
-		
-		this._all[task.id] = task
-		
+	saveTasksFromArray(tasks = []) {
+		tasks.forEach((task) => {
+			this._all[task.id] = task
+		})
 	}
 
+	createTask(description = '') {
+		const task = new Task(description)
 
-
-
+		this._all[task.id] = task
+	}
 }
 
 module.exports = Tasks
